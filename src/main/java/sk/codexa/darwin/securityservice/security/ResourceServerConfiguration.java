53d0752ce01d72e95eeabc30ff414d1b2dc18996
Jaroslav Regec <jaroslav.regec@codexa.sk>
2017-10-05 15:18:58 +0200

    Removed csrf config from ResourceServerConfigurer

--- src/main/java/sk/codexa/darwin/securityservice/security/ResourceServerConfiguration.java
+++ src/main/java/sk/codexa/darwin/securityservice/security/ResourceServerConfiguration.java
@@ -9,11 +9,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        //TODO add csrf protection
-        http.csrf()/*.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()*/
-                .disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/api/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_TENANT_ADMIN')").anyRequest().authenticated()
                 .and()
                 .authorizeRequests()
